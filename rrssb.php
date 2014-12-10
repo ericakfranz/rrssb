8e688018e5f29bd3edad5ab6f5e209752bb577e1
socialmediawithpb <priyanka@socialmediawithpb.com>
2014-12-10 09:55:00 -0500

    Update rrssb.php
    Added functions to add buttons either to the top or bottom of posts.

--- rrssb.php
+++ rrssb.php
@@ -14,6 +14,71 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+include( 'rrssb_admin.php' );
+
+/* On Activation & Decativation */
+
+function activate_rrssb() {	
+	add_option('show_email' , 1);
+	add_option('show_facebook' , 1);
+	add_option('show_linkedin' , 1);
+	add_option('show_twitter' , 1);
+	add_option('show_reddit'	, 1);
+	add_option('show_google'	, 1);
+	add_option('show_pocket'	, 0);
+	add_option('show_github'	, 0);
+	add_option('show_instagram' , 0);
+	add_option('show_pinterest' , 0);
+	add_option('show_tumblr' , 0);
+	add_option('show_youtube' , 0);
+	
+	add_option('github_link'	, "");
+	add_option('instagram_link' , "");
+	add_option('pinterest_link', "");
+	add_option('tumblr_link'	, "");
+	add_option('youtube_link' , "");
+	
+	add_option('give_rrssb_credit' , 0);
+	add_option('show_buttons_below_post', 1);
+	add_option('show_buttons_above_post', 1);
+	add_option('use_rrssb_jquery' , 0);
+	add_option('help_improve_rrssb' , 0);
+	add_option('rrssb_css' , ".no-margin li {margin: 0!important;}");
+	add_option('rrssb_css_classes' , "no-margin");
+}
+
+function deactive_rrssb() {  
+  	delete_option('show_email');
+	delete_option('show_facebook');
+	delete_option('show_linkedin');
+	delete_option('show_twitter');
+	delete_option('show_reddit');
+	delete_option('show_google');
+	delete_option('show_pocket');
+	delete_option('show_github');
+	delete_option('show_instagram');
+	delete_option('show_pinterest');
+	delete_option('show_tumblr');
+	delete_option('show_youtube');
+	
+	delete_option('github_link');
+	delete_option('instagram_link');
+	delete_option('pinterest_link');
+	delete_option('tumblr_link');
+	delete_option('youtube_link');
+	
+	delete_option('give_rrssb_credit');
+	delete_option('show_buttons_below_post');
+	delete_option('show_buttons_above_post');
+	delete_option('use_rrssb_jquery');
+	delete_option('help_improve_rrssb');
+	delete_option('rrssb_css');
+	delete_option('rrssb_css_classes');
+}
+
+register_activation_hook(__FILE__, 'activate_rrssb');
+register_deactivation_hook(__FILE__, 'deactive_rrssb');
+
 /* CSS and JS */
 
 function rrssb_js()
@@ -42,104 +107,90 @@ add_action('wp_enqueue_scripts', 'rrssb_css' );
 
 /* Add shortcode */
 
-/*function rrssb_show_buttons_shortcode()
+function rrssb_show_buttons_shortcode()
 {
-	return rrssb_show_buttons_below_post("");
+	return rrssb_show_buttons("");
 }
-add_shortcode('rrssb', 'rrssb_show_buttons_shortcode');*/
+add_shortcode('rrssb', 'rrssb_show_buttons_shortcode');
+
+
+/* Add buttons */
+
+function rrssb_show_buttons($rrssb_content)
+{
+	$rrssb_content .= '
+		<style>
+			' . get_option('rrssb_css') . '
+		</style>
+		<div class="share-container clearfix">
+		<rrssb-ul class="rrssb-buttons clearfix ' . get_option('rrssb_css_classes') . '">';
+	
+	if ( 1 == get_option('show_facebook') ) 
+		$rrssb_content .= rrssb_facebook_btn();
+	if ( 1 == get_option('show_twitter') ) 
+		$rrssb_content .= rrssb_twitter_btn();
+	if ( 1 == get_option('show_google') ) 
+		$rrssb_content .= rrssb_google_btn();
+	if ( 1 == get_option('show_linkedin') ) 
+		$rrssb_content .= rrssb_linkedin_btn();
+	if ( 1 == get_option('show_reddit') ) 
+		$rrssb_content .= rrssb_reddit_btn();
+	if ( 1 == get_option('show_email') ) 
+		$rrssb_content .= rrssb_email_btn();
+	if ( 1 == get_option('show_pinterest') ) 
+		$rrssb_content .= rrssb_pinterest_btn();	
+	if ( 1 == get_option('show_instagram') ) 
+		$rrssb_content .= rrssb_instagram_btn();	
+	if ( 1 == get_option('show_youtube') ) 
+		$rrssb_content .= rrssb_youtube_btn();
+	if ( 1 == get_option('show_tumblr') ) 
+		$rrssb_content .= rrssb_tumblr_btn();
+	if ( 1 == get_option('show_pocket') ) 
+		$rrssb_content .= rrssb_pocket_btn();
+	if ( 1 == get_option('show_github') ) 
+		$rrssb_content .= rrssb_github_btn();
+	
+	$rrssb_content .= '</rrssb-ul>';
+	
+	if ( 1 == get_option('give_rrssb_credit') )
+		$rrssb_content .= '
+		<label><small>Buttons by 
+		<a href="https://wordpress.org/plugins/ridiculously-responsive-social-sharing-buttons/">RRSSB</a>
+		</small></label>';
+		
+	$rrssb_content .= '
+		</div>';
+    
+	return $rrssb_content;
+}
 
 
 /* Show buttons above post */
 
 function rrssb_show_buttons_above_post($content)
 {
-    if (is_single()) {
-        
-		//$content .= rrssb_hide_unitl_load();
-        $rrssb_content .= '
-			<style>
-			.no-margin li {margin: 0!important;}
-			.fix-line-height a {line-height: 1.3em;} 
-			</style>
-			<div class="share-container clearfix">
-			<!-- buttons start here -->
-			<rrssb-ul class="rrssb-buttons clearfix no-margin fix-line-height">';
-
-	
-	/*
-	 * Edit below to reorder the buttons.
-	 * NOTE: Lines prefixed with `//` are ignored.
-	 *
-	 **/
- 
-        $rrssb_content .= rrssb_facebook_btn();
-        $rrssb_content .= rrssb_twitter_btn();
-        $rrssb_content .= rrssb_google_btn();
-        $rrssb_content .= rrssb_linkedin_btn(); 
-        $rrssb_content .= rrssb_reddit_btn();
-        $rrssb_content .= rrssb_email_btn();
-        // $rrssb_content .= rrssb_github_btn();
-        // $rrssb_content .= rrssb_instagram_btn();
-        // $rrssb_content .= rrssb_pinterest_btn();
-        // $rrssb_content .= rrssb_tumblr_btn();
-        // $rrssb_content .= rrssb_youtube_btn();
-	
-        
-        $rrssb_content .= '
-			</rrssb-ul>
-			<!-- buttons end here -->
-			</div>';
-	$rrssb_content .= $content;
-    }
-    return $rrssb_content;
+	if ( is_single() && ( 1 == get_option('show_buttons_above_post') ) ) {
+		$rrssb_content = rrssb_show_buttons("");
+		$rrssb_content .= $content;
+		return $rrssb_content;
+	}
+	else {
+		return $content;
+	}
 }
-
 add_filter('the_content', 'rrssb_show_buttons_above_post');
 
-function rrssb_show_buttons_below_post($content)
+/* Show buttons below post */
+
+function rrssb_show_buttons_below_post($rrssb_content)
 {
-    if (is_single()) {
-        
-		//$content .= rrssb_hide_unitl_load();
-        $rrssb_content .= '
-			<style>
-			.no-margin li {margin: 0!important;}
-			.fix-line-height a {line-height: 1.3em;} 
-			</style>
-			<div class="share-container clearfix">
-			<!-- buttons start here -->
-			<rrssb-ul class="rrssb-buttons clearfix no-margin fix-line-height">';
-
-	
-	/*
-	 * Edit below to reorder the buttons.
-	 * NOTE: Lines prefixed with `//` are ignored.
-	 *
-	 **/
- 
-        $rrssb_content .= rrssb_facebook_btn();
-        $rrssb_content .= rrssb_twitter_btn();
-        $rrssb_content .= rrssb_google_btn();
-        $rrssb_content .= rrssb_linkedin_btn(); 
-        $rrssb_content .= rrssb_reddit_btn();
-        $rrssb_content .= rrssb_email_btn();
-        // $rrssb_content .= rrssb_github_btn();
-        // $rrssb_content .= rrssb_instagram_btn();
-        // $rrssb_content .= rrssb_pinterest_btn();
-        // $rrssb_content .= rrssb_tumblr_btn();
-        // $rrssb_content .= rrssb_youtube_btn();
-	
-        
-        $rrssb_content .= '
-			</rrssb-ul>
-			<!-- buttons end here -->
-			</div>';
-	$content .= $rrssb_content;
-    }
-    return $content;
+    if ( is_single() && ( 1 == get_option('show_buttons_below_post') ) ) {
+		$rrssb_content = rrssb_show_buttons($rrssb_content);
+	}
+	return $rrssb_content;
 }
-
 add_filter('the_content', 'rrssb_show_buttons_below_post');
+
 
 /* Hide the buttons until after everything loads.
  * This prevents the buttons from displaying before
@@ -162,7 +213,7 @@ function rrssb_hide_unitl_load()
 	return $hide_buttons . $show_buttons;
 }
 
-/* Functions for each different kind of button */
+/* Functions for each button */
 
 function rrssb_email_btn()
 {
@@ -175,6 +226,7 @@ function rrssb_email_btn()
 		<span class="rrssb-text">email</span></a></li>';
     	return $rrssb_content;
 }
+
 function rrssb_facebook_btn()
 {
 	$icon = file_get_contents('icons/facebook.svg',true);
@@ -186,6 +238,7 @@ function rrssb_facebook_btn()
 		<span class="rrssb-text">facebook</span></a></li>';
     	return $rrssb_content;
 }
+
 function rrssb_linkedin_btn()
 {
 	$icon = file_get_contents('icons/linkedin.svg',true);
@@ -197,6 +250,7 @@ function rrssb_linkedin_btn()
 		<span class="rrssb-text">linkedin</span></a></li>';
     return $rrssb_content;
 }
+
 function rrssb_twitter_btn()
 {
 	$icon = file_get_contents('icons/twitter.svg',true);
@@ -232,6 +286,7 @@ function rrssb_google_btn()
 		<span class="rrssb-text">google+</span></a></li>';
     	return $rrssb_content;
 }
+
 function rrssb_github_btn()
 {
 	$icon = file_get_contents('icons/github.svg',true);
@@ -243,6 +298,7 @@ function rrssb_github_btn()
 		<span class="rrssb-text">github</span></a></li>';
     	return $rrssb_content;
 }
+
 function rrssb_instagram_btn()
 {
 	$icon = file_get_contents('icons/instagram.svg',true);
@@ -254,6 +310,7 @@ function rrssb_instagram_btn()
 		<span class="rrssb-text">instagram</span></a></li>';
     	return $rrssb_content;
 }
+
 function rrssb_pinterest_btn()
 {
 	$icon = file_get_contents('icons/pinterest.svg',true);
@@ -265,6 +322,7 @@ function rrssb_pinterest_btn()
 		<span class="rrssb-text">pinterest</span></a></li>';
     	return $rrssb_content;
 }
+
 function rrssb_tumblr_btn()
 {
 	$icon = file_get_contents('icons/tumblr.svg',true);
@@ -276,6 +334,7 @@ function rrssb_tumblr_btn()
 		<span class="rrssb-text">tumblr</span></a></li>';
     	return $rrssb_content;
 }
+
 function rrssb_youtube_btn()
 {
 	$icon = file_get_contents('icons/youtube.svg',true);
@@ -288,7 +347,16 @@ function rrssb_youtube_btn()
     	return $rrssb_content;
 }
 
-
+function rrssb_pocket_btn()
+{
+	$icon = file_get_contents('icons/pocket.svg',true);
+	$rrssb_content = '<li class="rrssb-pocket">
+		<a href="https://getpocket.com/save?url=' . urlencode( get_permalink() ) . '" class="popup">
+		<span class="rrssb-icon">
+		' . $icon . '
+		<span class="rrssb-text">pocket</span></a></li>';
+	return $rrssb_content;
+}
 
 /* END FILE */
 ?>
